c91533a2f1b1b68fb761e3ff444a15913292c642
voodoocjl <voodoocjl@hotmail.com>
2023-08-04 22:12:32 +0800

    print tree

--- exam_tree.py
+++ exam_tree.py
@@ -13,23 +13,5 @@ if __name__ == '__main__':
         node_path = os.path.join(state_path, files[-1])
         # node_path = 'results/mcts_agent_4000'
         with open(node_path, 'rb') as json_data:
-            agent = pickle.load(json_data)
-        print("\nresume searching,", agent.ITERATION, "iterations completed before")
-        print("=====>loads:", len(agent.nodes), "nodes")
-        print("=====>loads:", len(agent.samples), "samples")
-        print("=====>loads:", len(agent.DISPATCHED_JOB), "dispatched jobs")
-        print("=====>loads:", len(agent.TASK_QUEUE), "task_queue jobs from node:", agent.sample_nodes[0])
-        
-        print("\nclear the data in nodes...")
-        agent.reset_node_data()
-        print("finished")
-
-        print("\npopulate prediction data...")
-        agent.populate_prediction_data()
-        print("finished")
-
-        print("\npredict and partition nets in search space...")
-        agent.predict_nodes()
-        agent.check_leaf_bags()
-        print("finished")
+            agent = pickle.load(json_data)              
         agent.print_tree()
