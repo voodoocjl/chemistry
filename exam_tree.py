ba5769d78f4e88675ccdc577927ea0396275b503
voodoocjl <voodoocjl@hotmail.com>
2023-08-04 23:54:24 +0800

    print tree

--- exam_tree.py
+++ exam_tree.py
@@ -4,14 +4,15 @@ import json
 import torch
 from MCTS_chemisty import MCTS
 
-if __name__ == '__main__':
-    # set random seed
-    state_path = 'results'
-    files = os.listdir(state_path)
-    if files:
-        files.sort(key=lambda x: os.path.getmtime(os.path.join(state_path, x)))
-        node_path = os.path.join(state_path, files[-1])
-        # node_path = 'results/mcts_agent_4000'
-        with open(node_path, 'rb') as json_data:
-            agent = pickle.load(json_data)              
-        agent.print_tree()
+state_path = 'results/agent'
+files = os.listdir(state_path)
+if files:
+    files.sort(key=lambda x: os.path.getmtime(os.path.join(state_path, x)))
+    # node_path = os.path.join(state_path, files[-1])
+    node_path = os.path.join(state_path, files[0])
+    # node_path = 'results/mcts_agent_4000'
+    with open(node_path, 'rb') as json_data:
+        agent = pickle.load(json_data)              
+    agent.print_tree()
+    print('CP = ', agent.Cp)
+    print(agent.nodes[0].classifier.model)
