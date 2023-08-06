3aea0a3e68c8478a80a11f52dedd529d6096cf87
voodoocjl <voodoocjl@hotmail.com>
2023-08-07 00:47:35 +0800

    Update Classifier.py

--- Classifier.py
+++ Classifier.py
@@ -44,9 +44,9 @@ class Classifier:
         self.input_dim        = input_dim
         self.training_counter = 0
         self.node_layer       = ceil(log2(node_id + 2) - 1)
-        self.hidden_dims      =  [16, 20, 24, 28, 32]   #[5, 6, 7, 8, 9]
-        self.model            = Encoder(input_dim, self.hidden_dims[self.node_layer], 1)
-        # self.model            = Enco_Conv_Net(8, 7)
+        self.hidden_dims      =  [5, 6, 7, 8, 9]  #[16, 20, 24, 28, 32]
+        # self.model            = Encoder(input_dim, self.hidden_dims[self.node_layer], 1)
+        self.model            = Enco_Conv_Net(4, 7)
         if torch.cuda.is_available():
             self.model.cuda()
         self.loss_fn          = nn.MSELoss()
