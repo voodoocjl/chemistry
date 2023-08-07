4014893e16cd3efd2cdead260d15580e36c7dbcf
voodoocjl <voodoocjl@hotmail.com>
2023-08-07 11:11:21 +0800

    print all trees

--- exam_tree.py
+++ exam_tree.py
@@ -7,12 +7,13 @@ from MCTS_chemisty import MCTS
 state_path = 'results/agent'
 files = os.listdir(state_path)
 if files:
-    files.sort(key=lambda x: os.path.getmtime(os.path.join(state_path, x)))
-    # node_path = os.path.join(state_path, files[-1])
-    node_path = os.path.join(state_path, files[0])
-    # node_path = 'results/mcts_agent_4000'
-    with open(node_path, 'rb') as json_data:
-        agent = pickle.load(json_data)              
-    agent.print_tree()
-    print('CP = ', agent.Cp)
-    print(agent.nodes[0].classifier.model)
+    # files.sort(key=lambda x: os.path.getmtime(os.path.join(state_path, x)))
+    # # node_path = os.path.join(state_path, files[-1])
+    for file in files:
+        node_path = os.path.join(state_path, file)
+        # node_path = 'results/mcts_agent_4000'
+        with open(node_path, 'rb') as json_data:
+            agent = pickle.load(json_data)              
+        agent.print_tree()
+        print('CP = ', agent.Cp)
+        print(agent.nodes[0].classifier.model)
